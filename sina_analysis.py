243da36da43f41ea973d8cd81c3d5e79ed09fcca
allen <liuzhidong_1985@hotmail.com>
2017-07-26 06:49:02 +0800

    fix a bug

--- sina_analysis.py
+++ sina_analysis.py
@@ -1,14 +1,13 @@
-analURL = "http://vip.stock.finance.sina.com.cn/q/go.php/vIR_RatingNewest/index.phtml?p={}"
+analURL = "http://vip.stock.finance.sina.com.cn/q/go.php/vIR_RatingNewest/index.phtml"
 
 import tushare_wrapper as tuw
 from pyquery import PyQuery as pq
-import requests
 
-resPyQuery = pq(requests.get(analURL.format(1)).content)
 
-res = resPyQuery('tr')
-print(len(res))
-for i in range(1, len(res)):
-    print(res("td").eq(13*i).text())
+respyquery = pq(url=analURL)
+stock = respyquery('tr')
+
+for i in range(1, len(stock)):
+    print(stock("td").eq(13*i).text())
 
 
