fe9ee7735d0632fdd25f18ccddc32141409d2ea4
allen <liuzhidong_1985@hotmail.com>
2017-07-31 22:07:02 +0800

    add db change

--- sina_analysis.py
+++ sina_analysis.py
@@ -2,7 +2,10 @@ analURL = "http://vip.stock.finance.sina.com.cn/q/go.php/vIR_RatingNewest/index.
 
 import tushare_wrapper as tuw
 from pyquery import PyQuery as pq
+import pymysql
 
+
+conn = pymysql.connect(host="127.0.0.1",  user='root', password='355585', db='allen')
 
 respyquery = pq(url=analURL)
 stock = respyquery('tr')
@@ -11,3 +14,9 @@ for i in range(1, len(stock)):
     print(stock("td").eq(13*i).text())
 
 
+cur = conn.cursor()
+cur.execute("SELECT * FROM city")
+for r in cur:
+    print(r)
+cur.close()
+conn.close()
