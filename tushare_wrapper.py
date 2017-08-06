bda6a0f40cee8e4fad8ae41fd8a0e4dddeb31b72
allen <liuzhidong_1985@hotmail.com>
2017-08-06 20:59:21 +0800

    some change

--- tushare_wrapper.py
+++ tushare_wrapper.py
@@ -4,14 +4,16 @@ import datetime
 
 
 def get_stock_rate(s_code, s_start, workdays):
-    stock = ts.get_h_data(code=s_code, autype="hfq", start=s_start,
-                          end=str(datetime.datetime.strptime(s_start, "%Y/%m/%d").date() +
-                                  datetime.timedelta(days=workdays)))
+    try:
+        stock = ts.get_h_data(code=s_code, autype="hfq", start=s_start,
+                              end=str(datetime.datetime.strptime(s_start, "%Y/%m/%d").date() +
+                                      datetime.timedelta(days=workdays)))
 
-    end = stock['close'][0]
-    start = stock['close'][len(stock)-1]
-
+        end = stock['close'][0]
+        start = stock['close'][len(stock)-1]
+    except:
+        return 0
     return (end-start)/start
 
 
-#print(get_stock_rate('000651', '2017/01/01', 30))
+print(get_stock_rate('300087', '2017/07/28', 1))
