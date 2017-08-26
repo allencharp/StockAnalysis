07f3e2e3098fd419325b01547ce50fc02da2a387
allen <liuzhidong_1985@hotmail.com>
2017-08-26 18:28:16 +0800

    sql changed

--- SQL/db_scheam.sql
+++ SQL/db_scheam.sql
@@ -10,4 +10,18 @@ CREATE TABLE analysis
     analyst NVARCHAR(100),
     market NVARCHAR(20),
     date_time VARCHAR(20)
+);
+
+DROP TABLE IF EXISTS analysis_per_score;
+CREATE TABLE analysis_per_score
+(
+	item INT(11) PRIMARY KEY AUTO_INCREMENT,
+    organization NVARCHAR(100) NOT NULL,
+    analyst NVARCHAR(100) NOT NULL,
+    stock_code VARCHAR(10) NOT NULL,
+    date_time VARCHAR(20) NOT NULL,
+    tendays_value INT NULL
 )
+AS
+SELECT organization, analyst, stock_code,date_time
+FROM analysis GROUP BY organization, analyst,stock_code,date_time
